a12f8a551be1d447032e82f1a248c620b34dbc91
mishra file <mishrapratyaksh82@gmail.com>
2026-08-09 19:56:07 +0530

    Render deployment fix

--- campus-placement-portal/backend/index.js
+++ campus-placement-portal/backend/index.js
@@ -1,43 +1,55 @@
 require("dotenv").config();
 
 const express = require("express");
-const connectDB = require("./config/db");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const app = express();
 
-app.use(express.json());
-app.use(cookieParser());
-const PORT = process.env.PORT || 2500;
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+const profileRoutes = require("./routes/profileRoutes");
+const jobRoutes = require("./routes/jobRoutes");
+const companyRoutes = require("./routes/companyRoutes");
+const searchJobsRoutes = require("./routes/searchJobsRoutes");
+const notificationRoutes = require("./routes/notificationRoutes");
+const savedJobRoutes = require("./routes/savedJobRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
+const applicationRoutes = require("./routes/applicationRoutes");
+
+const app = express();
 
 connectDB();
 
-const authRoutes = require("./routes/authRoutes");
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Routes
 app.use("/api/auth", authRoutes);
-
-const profileRoutes = require("./routes/profileRoutes");
 app.use("/api/profile", profileRoutes);
-
-const jobRoutes = require("./routes/jobRoutes");
 app.use("/api/job", jobRoutes);
-
-const companyRoutes = require("./routes/companyRoutes");
 app.use("/api/company", companyRoutes);
-
-const searchJobsRoutes = require("./routes/searchJobsRoutes");
 app.use("/api/search", searchJobsRoutes);
-
-const notificationRoutes = require("./routes/notificationRoutes");
 app.use("/api/notifications", notificationRoutes);
-
-const savedJobRoutes = require("./routes/savedJobRoutes");
 app.use("/api/saved-jobs", savedJobRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/applications", applicationRoutes);
 
 app.get("/", (req, res) => {
-  res.json({
+  res.status(200).json({
     success: true,
-    message: "Campus Placement Portal API Running",
+    message: "Campus Placement Portal API Running ",
   });
 });
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(` Server running on port ${PORT}`);
