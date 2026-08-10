1c0d05fb6491ba453af2c56c31aefa811b89cfc8
mishra file <mishrapratyaksh82@gmail.com>
2026-08-10 20:29:39 +0530

    Fix authentication and resume upload

--- campus-placement-portal/backend/routes/profileRoutes.js
+++ campus-placement-portal/backend/routes/profileRoutes.js
@@ -1,20 +1,24 @@
 const express = require("express");
+
 const upload = require("../middleware/upload");
+
 const {
   checkForAuthenticationCookie,
 } = require("../middleware/authMiddleware");
-const router = express.Router();
+
 const {
   getProfile,
   updateProfile,
   uploadResume,
 } = require("../controllers/profileController");
 
+const router = express.Router();
+
 router.get("/", checkForAuthenticationCookie("token"), getProfile);
 
 router.put("/", checkForAuthenticationCookie("token"), updateProfile);
 
-router.put(
+router.post(
   "/upload-resume",
   checkForAuthenticationCookie("token"),
   upload.single("resume"),
